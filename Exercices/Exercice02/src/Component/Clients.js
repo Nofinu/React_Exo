fd29f78f6fb56bf8a640aa4d2b5dc803db35a28c
Léo Lhomme <leolhommeo@gmail.com>
2023-02-09 14:14:51 +0100

    ajout de ternaire

--- Exercices/Exercice02/src/Component/Clients.js
+++ Exercices/Exercice02/src/Component/Clients.js
@@ -1,23 +1,34 @@
-import {Component} from "react"
+// import {Component} from "react"
 import { Adresse } from "./Adresse.js"
 
 
-export class Clients extends Component{
-  constructor(props){
-    super()
-    this.state = {}
-  }
+// export class Clients extends Component{
+//   constructor(props){
+//     super(props)
+//     this.state = {}
+//   }
 
-  render(){
-    const {nom,prenom,telephone,statut,adresse} = this.props.client
-    return(
-    <table className="tableContact">
-      <tbody>
-        <tr className={statut? "" : "red"}><td>nom : </td><td>{nom}</td><td>prenom : </td><td>{prenom}</td><td>telephone :</td><td>{telephone}</td>{statut? <td>Actif</td> : <td>Inactif</td>}</tr>
-        <Adresse adresse={adresse} statut={statut}></Adresse>
-      </tbody>
-  </table>
-    )
-  }
+//   render(){
+//     const {nom,prenom,telephone,statut,adresse} = this.props.client
+//     return(
+//     <table className="tableContact">
+//       <tbody>
+//         <tr className={statut? "" : "red"}><td>nom : </td><td>{nom}</td><td>prenom : </td><td>{prenom}</td><td>telephone :</td><td>{telephone}</td>{statut? <td>Actif</td> : <td>Inactif</td>}</tr>
+//         <Adresse adresse={adresse} statut={statut}></Adresse>
+//       </tbody>
+//   </table>
+//     )
+//   }
+// }
+
+export function Clients (props){
+  const {nom,prenom,telephone,statut,adresse} = props.client
+  return(
+  <table className="tableContact">
+    <tbody>
+      <tr className={statut? "" : "red"}><td>nom : </td><td>{nom}</td><td>prenom : </td><td>{prenom}</td><td>telephone :</td><td>{telephone}</td>{statut? <td>Actif</td> : <td>Inactif</td>}</tr>
+      <Adresse adresse={adresse} statut={statut}></Adresse>
+    </tbody>
+</table>
+  )
 }
-
