38ed7e68d550900567deeac085021ee54ab73b35
Léo Lhomme <leolhommeo@gmail.com>
2023-02-13 11:13:24 +0100

    ajout d'un objet panier

--- Tp/tp01/src/Component/GlobalContainer.js
+++ Tp/tp01/src/Component/GlobalContainer.js
@@ -13,21 +13,22 @@ export class GlobalContainer extends Component{
                 titre : "Article1",
                 description : "La descirption de l'article n°1",
                 prix : 35,
-                quantite:0
+                //quantite:0
             },{
                 id: "0000002",
                 titre : "Article2",
                 description : "La descirption de l'article n°2",
                 prix : 18,
-                quantite:0
+                //quantite:0
             },{
                 id: "0000003",
                 titre : "Article3",
                 description : "La descirption de l'article n°3",
                 prix : 55,
-                quantite:0
+                //quantite:0
             },
         ],
+        panier:[],
         statutModal : false
         }
     }
@@ -35,19 +36,28 @@ export class GlobalContainer extends Component{
     refreshTotalPrix=()=>{
         let tmpStateprix = {...this.state}
         tmpStateprix.totalPrix = 0
-        tmpStateprix.items.forEach(item =>{
-            tmpStateprix.totalPrix+= item.quantite*item.prix
+        tmpStateprix.panier.forEach(article =>{
+            tmpStateprix.totalPrix+= article.quantite*article.prix
         })
         this.setState({...tmpStateprix})
     }
 
     ajouterArticle=(id)=>{
         let tmpState = {...this.state}
-        tmpState.items.forEach(item =>{
-            if(id ===item.id){
-                item.quantite++
+        let test = false
+        tmpState.panier.forEach(article =>{
+            if(id ===article.id){
+                article.quantite++
+                test = true
             }
         })
+        if(!test){
+            tmpState.items.forEach(item =>{
+                if(id === item.id){
+                    tmpState.panier.push({...item,quantite : 1})
+                }
+            })  
+        }
         this.setState({...tmpState})
         this.refreshTotalPrix()
         
@@ -55,9 +65,12 @@ export class GlobalContainer extends Component{
 
     supprimerUnArticle=(id)=>{
         let tmpState = {...this.state}
-        tmpState.items.forEach(item =>{
-            if(id === item.id){
-                item.quantite--
+        tmpState.panier.forEach(article =>{
+            if(id === article.id){
+                article.quantite--
+                if(article.quantite === 0){
+                    tmpState.panier.splice(tmpState.panier.indexOf(article),1)
+                }
             }
         })
         this.setState({...tmpState})
@@ -67,9 +80,9 @@ export class GlobalContainer extends Component{
 
     supprimerAllArticle=(id)=>{
         let tmpState = {...this.state}
-        tmpState.items.forEach(item =>{
-            if(id === item.id){
-                item.quantite=0
+        tmpState.panier.forEach(article =>{
+            if(id === article.id){
+                tmpState.panier.splice(tmpState.panier.indexOf(article),1)
             }
         })
         this.setState({...tmpState})
@@ -93,8 +106,8 @@ export class GlobalContainer extends Component{
 
     render(){  
         let nbrArticle =0
-        this.state.items.forEach(item=>{
-            nbrArticle+= item.quantite
+        this.state.panier.forEach(article=>{
+            nbrArticle+= article.quantite
         })
         return (
             <div className="globalContainer">
@@ -105,7 +118,7 @@ export class GlobalContainer extends Component{
                     <DisplayPanier nbrArticle={nbrArticle} totalprix={this.state.totalPrix} gestionModal={this.gestionModal}></DisplayPanier>
                 </div>
                 <div className={this.state.statutModal?"on":"off"}>
-                    <ModalPanier items={this.state.items} gestionModal={this.gestionModal} supprimerUnArticle={this.supprimerUnArticle} supprimerAllArticle={this.supprimerAllArticle} nbrArticle={nbrArticle} totalprix={this.state.totalPrix}></ModalPanier>
+                    <ModalPanier panier={this.state.panier} gestionModal={this.gestionModal} supprimerUnArticle={this.supprimerUnArticle} supprimerAllArticle={this.supprimerAllArticle} nbrArticle={nbrArticle} totalprix={this.state.totalPrix}></ModalPanier>
                 </div>
             </div>
             )
