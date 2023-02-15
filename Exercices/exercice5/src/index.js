5470cd1361c864ab47d7defdea7ca72bd3bf2c40
Léo Lhomme <leolhommeo@gmail.com>
2023-02-15 15:08:04 +0100

    ajout exercice5 exo Chronometre

--- Exercices/exercice5/src/index.js
+++ Exercices/exercice5/src/index.js
@@ -1,14 +1,11 @@
-import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { MainComponent } from './Component/MainComponent';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <MainComponent></MainComponent>
 );
 
 // If you want to start measuring performance in your app, pass a function
