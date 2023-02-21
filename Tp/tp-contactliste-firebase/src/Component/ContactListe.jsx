51a37622f36aad5c6fa76e75abdada94cb55b8f0
Léo Lhomme <leolhommeo@gmail.com>
2023-02-21 16:56:55 +0100

    ajout du refresh token automatique tout les 10 mins

--- Tp/tp-contactliste-firebase/src/Component/ContactListe.jsx
+++ Tp/tp-contactliste-firebase/src/Component/ContactListe.jsx
@@ -7,7 +7,7 @@ export const ContactListe=(props)=>{
     <div id="contactContainer">
       <div id="headerContactListe">
       <h1>Contacts</h1>
-      <button onClick={props.openModalForm} className="fa-solid fa-plus" >Ajouter</button>
+      <button onClick={props.openModalForm} className="fa-solid fa-user-plus" ></button>
       </div>
       <hr />
       {props.contactListe.length > 0&& props.contactListe.map(elem => <ConstactItem SuprContact={props.SuprContact} openModalFormModif={props.openModalFormModif} key={elem.id} contact={elem}/>)}
