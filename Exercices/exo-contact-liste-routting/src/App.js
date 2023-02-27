35748020423086a638f98e69aa6867ce0c9665a1
Léo Lhomme <leolhommeo@gmail.com>
2023-02-27 17:25:26 +0100

    ajout des exo

--- Exercices/exo-contact-liste-routting/src/App.js
+++ Exercices/exo-contact-liste-routting/src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
+import { NavLink, Outlet } from "react-router-dom";
+import { useState } from 'react';
 
 function App() {
+
+  const [stateNav,setStateNAv]=useState("")
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <nav>
+          <h2>eWebsite</h2>
+          <NavLink className={stateNav === "" ? "navlinkon":"navlinkoff"} to="/" onClick={()=>setStateNAv("")}>Home</NavLink>
+          <NavLink className={stateNav === "contacts" ? "navlinkon":"navlinkoff"} to="/contacts" onClick={()=>setStateNAv("contacts")}>Contacts </NavLink>
+        </nav>
       </header>
+      <main>
+        <Outlet/>
+      </main>
     </div>
   );
 }
